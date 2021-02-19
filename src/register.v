ce0a9b110154caf2fdd99564e2abcfe268edbb1e
MatiF100 <mateuszfesz@wp.pl>
2021-02-19 22:02:00 +0100

    Added comments to registries, for readability

--- src/register.v
+++ src/register.v
@@ -1,34 +1,36 @@
 `timescale 1ns / 1ps
-
-module register(
-    );
-	 
-
-endmodule
-
-
+//Following link leads to the block diagram of the 6502, on which all registers are based
+//Original creator of the diagram:  Donald F. Hanson
+//Original document: https://projects.ncsu.edu/wcae//WCAE1/hanson.pdf
+//https://www.witwright.com/DonPub/6502-Block-Diagram.pdf
+//Most of the names of inputs/outputs corresponds to datapaths/markings on this schematic
+//If any differences/special circumstances happen to arise, they should be explicitly noted "in place",
+//as it is done in the following module
 
 module reg_XY( //module for x and y registers
-    input wire LOAD,
-    input wire BUS_ENABLE,
-    input wire [7:0] DATA,
-    output reg [7:0] OUT
-    );
+	//Possible to use as ADDRESS BUS, PRECODE register as well,
+	//Just lock BUS_ENABLE high
+	input wire LOAD,
+	input wire BUS_ENABLE,
+	input wire [7:0] DATA,
+	output reg [7:0] OUT
+	);
 
 	reg [7:0] register;
 	
 
-
+	//@(*), means that evaluation happens if any of revelant signals gets changed
 	always@(*)
 	begin
-		 if(LOAD)
-			  register = DATA;
-		 if(BUS_ENABLE)
-			  OUT = register;
+		if(LOAD)
+			register = DATA;
+		if(BUS_ENABLE)
+			OUT = register;
 	end
 
 
 endmodule
+
 
 module reg_ACC( //module for accumulator register
 	//prefix tells the name of source/target datapath
@@ -61,11 +63,11 @@ endmodule
 module reg_S( //module for Stack Pointer Register
 	input wire RELOAD,
 	input wire SB_LOAD,
-   input wire SB_BUS_ENABLE,
-   input wire ADL_BUS_ENABLE,
-   input wire [7:0] SB_DATA,
-   output reg [7:0] SB_OUT,
-   output reg [7:0] ADL_OUT
+	input wire SB_BUS_ENABLE,
+	input wire ADL_BUS_ENABLE,
+	input wire [7:0] SB_DATA,
+	output reg [7:0] SB_OUT,
+	output reg [7:0] ADL_OUT
    );
 
 	reg [7:0] register;
@@ -83,6 +85,5 @@ module reg_S( //module for Stack Pointer Register
 		if(RELOAD)
 			register = register;
 	end
-
 
 endmodule
